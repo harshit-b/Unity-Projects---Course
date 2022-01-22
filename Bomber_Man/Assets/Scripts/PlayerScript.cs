13825de6177fd0e46d41798d73c2eb771ad22bab
Harshit Bhargava <harshit.bhargava@ttu.edu>
2022-01-21 21:06:25 -0600

    Finally added first project

---
+++ Bomber_Man/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float speed = 10f;
+    private float minX = -2.5f;
+    private float maxX = 2.5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        float h = Input.GetAxis("Horizontal");
+        Vector2 currentPosition = transform.position;
+        if (h>0) {
+            //going to the right side
+            currentPosition.x += speed * Time.deltaTime;
+        }
+
+        else if (h<0)
+        {
+            //going to the left side 
+            currentPosition.x -= speed * Time.deltaTime;
+        }
+
+        if (currentPosition.x <minX)
+        {
+            currentPosition.x = minX;
+        }
+
+        if (currentPosition.x > maxX)
+        {
+            currentPosition.x = maxX;
+        }
+        transform.position = currentPosition;
+    }
+
+    void OnTriggerEnter2D(Collider2D target)
+    {
+        if (target.tag == "Bomb")
+        {
+            Time.timeScale = 0f;
+        }
+    }
+}
